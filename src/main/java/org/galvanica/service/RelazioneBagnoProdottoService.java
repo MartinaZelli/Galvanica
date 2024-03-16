671a8d7b816c85751e5dacd6bcaef1a3784b4092
MartinaZelli <martinazelli@live.it>
2024-03-16 17:27:02 +0100

    scritta (da testare) RelazioneBagnoProdottoService

--- src/main/java/org/galvanica/service/RelazioneBagnoProdottoService.java
+++ src/main/java/org/galvanica/service/RelazioneBagnoProdottoService.java
@@ -26,6 +26,10 @@ public class RelazioneBagnoProdottoService implements ICRUDService<RelazioneBagn
 
     @Override
     public RelazioneBagnoProdottoDto inserisci(RelazioneBagnoProdottoDto elemento) {
+        if (elemento.getIdRelazione() != null) {
+            throw new RuntimeException(
+                    "l'id relazione NON deve essere valorizzato, è autoincrementale.");
+        }
         if (elemento.getIdProdotto() == null) {
             throw new RuntimeException(
                     "l'id prodotto deve essere valorizzato.");
@@ -52,6 +56,7 @@ public class RelazioneBagnoProdottoService implements ICRUDService<RelazioneBagn
         relazioneBagnoProdotto = relazioneBagnoProdottoRepository.save(
                 relazioneBagnoProdotto);
         return RelazioneBagnoProdottoDto.builder()
+                .idRelazione(relazioneBagnoProdotto.getIdRelazione())
                 .idBagno(relazioneBagnoProdotto.getBagno().getIdBagno())
                 .idProdotto(relazioneBagnoProdotto.getProdotto().getIdProdotto())
                 .note(relazioneBagnoProdotto.getNote())
@@ -60,16 +65,65 @@ public class RelazioneBagnoProdottoService implements ICRUDService<RelazioneBagn
 
     @Override
     public Boolean elimina(long id) {
-        return null;
+        boolean risposta = relazioneBagnoProdottoRepository.findById(id).isPresent();
+        if (risposta) {
+            relazioneBagnoProdottoRepository.deleteById(id);
+        }
+        return risposta;
     }
 
     @Override
     public RelazioneBagnoProdottoDto aggiorna(RelazioneBagnoProdottoDto elemento, long id) {
-        return null;
+        Optional<RelazioneBagnoProdotto> relazioneBagnoProdottoOptional = relazioneBagnoProdottoRepository.findById(
+                id);
+        if (relazioneBagnoProdottoOptional.isEmpty()) {
+            throw new RuntimeException(
+                    "mettere un id corretto");
+        }
+        if (elemento.getIdProdotto() == null) {
+            throw new RuntimeException(
+                    "l'id prodotto deve essere valorizzato.");
+        }
+        if (elemento.getIdBagno() == null) {
+            throw new RuntimeException(
+                    "l'id bagno deve essere valorizzato.");
+        }
+        Optional<Prodotto> prodottoOptional = prodottoRepository.findById(elemento.getIdProdotto());
+        if (prodottoOptional.isEmpty()) {
+            throw new RuntimeException(
+                    "l'id del prodotto non esiste, correggere.");
+        }
+        Optional<Bagno> bagnoOptional = bagnoRepository.findById(elemento.getIdBagno());
+        if (bagnoOptional.isEmpty()) {
+            throw new RuntimeException(
+                    "l'id del bagno non esiste, correggere.");
+        }
+        RelazioneBagnoProdotto relazioneBagnoProdotto = relazioneBagnoProdottoOptional.get();
+        relazioneBagnoProdotto.setNote(elemento.getNote());
+        relazioneBagnoProdotto.setBagno(bagnoOptional.get());
+        relazioneBagnoProdotto.setProdotto(prodottoOptional.get());
+        relazioneBagnoProdotto = relazioneBagnoProdottoRepository.save(
+                relazioneBagnoProdotto);
+        return RelazioneBagnoProdottoDto.builder()
+                .idRelazione(relazioneBagnoProdotto.getIdRelazione())
+                .idBagno(relazioneBagnoProdotto.getBagno().getIdBagno())
+                .idProdotto(relazioneBagnoProdotto.getProdotto().getIdProdotto())
+                .note(relazioneBagnoProdotto.getNote())
+                .build();
     }
 
     @Override
     public Optional<RelazioneBagnoProdottoDto> ricercaId(long id) {
-        return Optional.empty();
+        Optional<RelazioneBagnoProdotto> relazioneBagnoProdottoOptional =
+                relazioneBagnoProdottoRepository.findById(id);
+
+        return relazioneBagnoProdottoOptional
+                .map(relazioneBagnoProdotto -> RelazioneBagnoProdottoDto.builder()
+                        .idRelazione(relazioneBagnoProdotto.getIdRelazione())
+                        .idBagno(relazioneBagnoProdotto.getBagno().getIdBagno())
+                        .idProdotto(relazioneBagnoProdotto.getProdotto()
+                                .getIdProdotto())
+                        .note(relazioneBagnoProdotto.getNote())
+                        .build());
     }
 }
