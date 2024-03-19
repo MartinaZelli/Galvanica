f2ea02ce56d5ab52e23c65743bfeb0a36b208916
MartinaZelli <martinazelli@live.it>
2024-03-19 21:52:28 +0100

    continuiamo a parlare di matematica....pare.. aggiunto metodo approssimazioneAggiunta

---
+++ src/main/java/org/galvanica/Math/MetodiArrotondamenti.java
@@ -0,0 +1,49 @@
+package org.galvanica.Math;
+
+public class MetodiArrotondamenti {
+    public static ScattiMath alimentazioneScattiApprossimata(int scattiAttuali,
+                                                             int scattiAlimentazione) {
+        ScattiMath risposta = new ScattiMath();
+        double moltiplicatoreReale = (double) scattiAttuali / scattiAlimentazione;
+        double moltiplicatoreFloor = Math.floor(moltiplicatoreReale);
+        double moltiplicatoreApprossimato = moltiplicatoreApprossimato(
+                moltiplicatoreReale,
+                moltiplicatoreFloor);
+        risposta.setRestoScatti((int) ((moltiplicatoreReale - moltiplicatoreApprossimato) * scattiAlimentazione));
+        risposta.setMoltiplicatoreAlimentazione(moltiplicatoreApprossimato);
+
+        return risposta;
+    }
+
+    private static double moltiplicatoreApprossimato(double moltiplicatoreReale,
+                                                     double moltiplicatoreFloor) {
+        double cifreDecimali = moltiplicatoreReale - moltiplicatoreFloor;
+        if (cifreDecimali < 0.4) {
+            return moltiplicatoreFloor;
+        }
+        if (cifreDecimali <= 0.9) {
+            return moltiplicatoreFloor + 0.5;
+        }
+        if (cifreDecimali > 0.9) {
+            return moltiplicatoreFloor + 1;
+        }
+        return 0;
+    }
+
+    public double approssimazioneAggiunta(double aggiunta) {
+        if (aggiunta < 100) {
+            double resto = aggiunta % 5;
+            return aggiunta - resto;
+        }
+        if (aggiunta < 500) {
+            double resto = aggiunta % 10;
+            return aggiunta - resto;
+        }
+        if (aggiunta < 1000) {
+            double resto = aggiunta % 50;
+            return aggiunta - resto;
+        }
+        double resto = aggiunta % 250;
+        return aggiunta - resto;
+    }
+}
