3beaf796a60aea4f408ea7f177af0b1c2136b9ac
Orfeo42 <lucio.rosadini87@gmail.com>
2024-03-30 19:20:51 +0100

    query brutta brutta per selezionare solo storici specifici per bagno e booleano concluso su StoricoGeneraleRepository

--- src/main/java/org/galvanica/repository/StoricoGeneraleRepository.java
+++ src/main/java/org/galvanica/repository/StoricoGeneraleRepository.java
@@ -1,7 +1,13 @@
 package org.galvanica.repository;
 
 import org.galvanica.model.StoricoGenerale;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface StoricoGeneraleRepository extends CrudRepository<StoricoGenerale, Long> {
+    @Query(value = "SELECT * FROM storico_generale WHERE concluso = ?1 AND bagno_id_bagno = ?2", nativeQuery = true)
+    List<StoricoGenerale> storicoGeneraleList(boolean concluso, Long idBagno);
+
 }
