ad8d40ca16ff700574f10cdf602cc2eca9363c29
Orfeo42 <lucio.rosadini87@gmail.com>
2024-04-03 15:08:45 +0200

    aggiunta gestione modifica bagno FE

--- src/main/java/org/galvanica/controller/FrontEndTestController.java
+++ src/main/java/org/galvanica/controller/FrontEndTestController.java
@@ -5,10 +5,13 @@ import org.galvanica.service.CRUD.BagnoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@Controller
+@Controller()
 public class FrontEndTestController {
 
     private final BagnoService bagnoService;
@@ -17,11 +20,26 @@ public class FrontEndTestController {
         this.bagnoService = bagnoService;
     }
 
-    @GetMapping("/elenco-bagni")
-    public String test(Model model) {
-        List<BagnoDto> bagni = bagnoService.getAllBagno();
 
+    @GetMapping("bagnoFe/elenco")
+    public String listaBagni(Model model) {
+        List<BagnoDto> bagni = bagnoService.getAllBagno();
         model.addAttribute("bagni", bagni);
-        return "test";
+        return "bagnoList";
     }
+
+    @GetMapping("bagnoFe/edit/{id}")
+    public String listaBagni(@PathVariable Long id, Model model) {
+        BagnoDto bagno = bagnoService.ricercaId(id).orElseThrow(RuntimeException::new);
+        model.addAttribute("bagno", bagno);
+        return "bagnoCard :: editCard";
+    }
+
+    @PutMapping(value = "bagnoFe/{id}")
+    public String aggiornaBagno(@RequestBody BagnoDto bagnoDto, @PathVariable Long id, Model model) {
+        BagnoDto bagno = bagnoService.aggiorna(bagnoDto, id);
+        model.addAttribute("bagno", bagno);
+        return "bagnoCard :: showCard";
+    }
+
 }
