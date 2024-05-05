8ff1e0cd7663c0de71ccd495742bb2a117c13acc
MartinaZelli <martinazelli@live.it>
2024-05-05 20:55:11 +0200

    Correzione invio dati da fe

--- src/main/java/org/galvanica/controller/OperazioniDiAlimentazione.java
+++ src/main/java/org/galvanica/controller/OperazioniDiAlimentazione.java
@@ -1,5 +1,7 @@
 package org.galvanica.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.galvanica.dto.AlimentazioneRisposta;
 import org.galvanica.service.CRUD.AlimentazioneService;
 import org.galvanica.service.CRUD.BagnoService;
 import org.galvanica.service.operazioniBagno.OperazioniAddStorico;
@@ -7,7 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 
@@ -34,15 +37,31 @@ public class OperazioniDiAlimentazione {
     @GetMapping("/scatti/preconto/{id}")
     @ResponseBody
     public String preconto(@PathVariable Long id, HttpServletRequest request) {
-        String paramName = "scatti_" + id;
-        Integer scatti = Integer.valueOf(request.getParameter(paramName));
-        Integer scattiAlimentazione = alimentazioneService.ricercaAlimentazioneByBagno(id)
+        String scattiValue = request.getParameter(String.valueOf(id));
+        if (scattiValue == null || scattiValue.isEmpty()) {
+            return String.format("%.2f", 0d);
+        }
+        double scatti = Double.parseDouble(scattiValue);
+
+        Integer scattiAlimentazione = alimentazioneService.ricercaAlimentazioneByBagno(
+                        id)
                 .stream()
                 .filter(alimentazione -> alimentazione.getScatti() != null)
                 .findFirst()
                 .orElseThrow().getScatti();
-        Double moltiplicatore = ((double) scatti / (double) scattiAlimentazione);
+        Double moltiplicatore = (scatti / (double) scattiAlimentazione);
         return String.format("%.2f", moltiplicatore);
     }
+
+    @PostMapping("/scatti")
+    public String calcoloAlimentazione(Model model,
+                                       @RequestBody Map<Long, Integer> mappaBagnoScatti) {
+        List<AlimentazioneRisposta> alimentazioneRispostaList =
+                operazioniAddStoricoService.scattiCalcolaAlimentazioneList(
+                        mappaBagnoScatti);
+        System.out.println(alimentazioneRispostaList);
+        return scatti(model);
+    }
+
 
 }
--- src/main/java/org/galvanica/dto/AlimentazioneRisposta.java
+++ src/main/java/org/galvanica/dto/AlimentazioneRisposta.java
@@ -3,13 +3,14 @@ package org.galvanica.dto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
-
+@ToString
 public class AlimentazioneRisposta {
 
     private Long idBagno;
