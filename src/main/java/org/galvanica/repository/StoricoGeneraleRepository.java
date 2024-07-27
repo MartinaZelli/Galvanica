f42b5e4ad54f3640b6b26ef05ddc2bbbd4b84690
MartinaZelli <martinazelli@live.it>
2024-07-27 18:22:55 +0200

    sistemate query StoricoGeneraleRepository

--- src/main/java/org/galvanica/repository/StoricoGeneraleRepository.java
+++ src/main/java/org/galvanica/repository/StoricoGeneraleRepository.java
@@ -9,16 +9,32 @@ import java.util.List;
 
 public interface StoricoGeneraleRepository extends CrudRepository<StoricoGenerale, Long> {
 
-    @Query(value = "SELECT * FROM storico_generale " +
-            "WHERE concluso = ?1 AND bagno_id_bagno = ?2 AND tipologia_aggiunta like ?3 " +
-            "ORDER BY data_creazione DESC", nativeQuery = true)
-    List<StoricoGenerale> storicoGeneraleDescList(boolean concluso, Long idBagno,
+    @Query(value = """
+            SELECT
+            *
+            FROM
+                storico_generale
+            WHERE
+                eseguito_generale = ?1
+              AND bagno_id_bagno = ?2
+              AND tipologia_aggiunta like ?3
+            ORDER BY data_creazione DESC
+            """, nativeQuery = true)
+    List<StoricoGenerale> storicoGeneraleDescList(boolean eseguito, Long idBagno,
                                                   TipologiaAggiunta tipologiaAggiunta);
 
-    @Query(value = "SELECT * FROM storico_generale " +
-            "WHERE bagno_id_bagno = ?1 AND tipologia_aggiunta like 'TEMPO' " +
-            "ORDER BY data_creazione DESC " +
-            "LIMIT 1", nativeQuery = true)
+
+    @Query(value = """
+            SELECT
+                *
+            FROM storico_generale
+            WHERE
+                bagno_id_bagno = ?1
+              AND tipologia_aggiunta like 'TEMPO'
+              AND not annullato_generale
+            ORDER BY data_creazione DESC
+            LIMIT 1
+            """, nativeQuery = true)
     StoricoGenerale storicoGeneraleTempoLast(Long idBagno);
 
     @Query(value = """
@@ -28,8 +44,9 @@ public interface StoricoGeneraleRepository extends CrudRepository<StoricoGeneral
             WHERE
                 bagno_id_bagno = ?1
             ORDER BY
-                data_creazione DESC, concluso
-            LIMIT ?2""", nativeQuery = true)
+                data_creazione DESC, eseguito_generale
+            LIMIT ?2
+            """, nativeQuery = true)
     List<StoricoGenerale> storicoGeneraleListByBagno(Long idBagno, Long limite);
 
     @Query(value = """
@@ -38,7 +55,7 @@ public interface StoricoGeneraleRepository extends CrudRepository<StoricoGeneral
             FROM storico_generale
             WHERE
                 bagno_id_bagno = ?1
-                AND tipologia_aggiunta like 'SCATTI'
+                AND tipologia_aggiunta  not like 'TEMPO'
             ORDER BY
                 data_creazione DESC
             LIMIT 1""", nativeQuery = true)
