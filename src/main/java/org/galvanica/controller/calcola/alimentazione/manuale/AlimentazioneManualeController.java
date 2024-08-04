d301dd24e4e4fc892642072c52c388be732f4025
orfeo42 <lucio.rosadini87@gmail.com>
2024-08-04 19:26:49 +0200

    gestione inserimento manuale con post dati al be

--- src/main/java/org/galvanica/controller/calcola/alimentazione/manuale/AlimentazioneManualeController.java
+++ src/main/java/org/galvanica/controller/calcola/alimentazione/manuale/AlimentazioneManualeController.java
@@ -20,69 +20,66 @@ import java.util.List;
 public class AlimentazioneManualeController {
 
 
-    private final BagnoService bagnoService;
-    private final ProdottoService prodottoService;
+	private final BagnoService bagnoService;
+	private final ProdottoService prodottoService;
 
-    public AlimentazioneManualeController(BagnoService bagnoService,
-                                          ProdottoService prodottoService) {
-        this.bagnoService = bagnoService;
-        this.prodottoService = prodottoService;
-    }
+	public AlimentazioneManualeController(BagnoService bagnoService, ProdottoService prodottoService) {
+		this.bagnoService = bagnoService;
+		this.prodottoService = prodottoService;
+	}
 
-    @GetMapping
-    public String primaPagina(Model model) {
-        model.addAttribute("bagnoList", bagnoService.findAllBagno());
-        return "calcola-alimentazione/manuale/prima-pagina";
-    }
+	@GetMapping
+	public String primaPagina(Model model) {
+		model.addAttribute("bagnoList", bagnoService.findAllBagno());
+		return "calcola-alimentazione/manuale/prima-pagina";
+	}
 
-    @GetMapping("/bagno")
-    public String selezionaBagno(Model model, @RequestParam int bagnoId) {
-        Bagno bagno = bagnoService.modelRicercaId(bagnoId);
-        model.addAttribute("bagno", bagno);
-        return "calcola-alimentazione/manuale/fragments :: bagnoSelect";
-    }
+	@GetMapping("/bagno")
+	public String selezionaBagno(Model model, @RequestParam int bagnoId) {
+		Bagno bagno = bagnoService.modelRicercaId(bagnoId);
+		model.addAttribute("bagno", bagno);
+		return "calcola-alimentazione/manuale/fragments :: bagnoSelect";
+	}
 
-    @GetMapping("/prodotto")
-    public String selezionaProdotto(Model model, @RequestParam int bagnoId) {
-        Bagno bagno = bagnoService.modelRicercaId(bagnoId);
-        //model.addAttribute("bagno", bagno);
-        List<Prodotto> prodottoList = bagno.getRelazioneBagnoProdottoList()
-                .stream()
-                .map(RelazioneBagnoProdotto::getProdotto)
-                .toList();
-        model.addAttribute("prodottoList", prodottoList);
-        return "calcola-alimentazione/manuale/fragments :: prodottoSelect";
-    }
+	@GetMapping("/prodotto")
+	public String selezionaProdotto(Model model, @RequestParam int bagnoId) {
+		Bagno bagno = bagnoService.modelRicercaId(bagnoId);
+		//model.addAttribute("bagno", bagno);
+		List<Prodotto> prodottoList =
+			bagno.getRelazioneBagnoProdottoList().stream().map(RelazioneBagnoProdotto::getProdotto).toList();
+		model.addAttribute("prodottoList", prodottoList);
+		return "calcola-alimentazione/manuale/fragments :: prodottoSelect";
+	}
 
-    @GetMapping("/prodotto/unita-misura")
-    public String selezionaUnitaMisura(Model model, @RequestParam int prodottoId) {
-        Prodotto prodotto = prodottoService.modelRicercaId(prodottoId);
-        List<UnitaDiMisura> unitaMisuraList = Arrays.stream(UnitaDiMisura.values())
-                .filter(um -> um.isSonoVolume() == prodotto.getSonoVolume())
-                .toList();
-        model.addAttribute("unitaMisuraList", unitaMisuraList);
-        return "calcola-alimentazione/manuale/fragments :: unitaMisura";
-    }
+	@GetMapping("/prodotto/unita-misura")
+	public String selezionaUnitaMisura(Model model, @RequestParam int prodottoId) {
+		Prodotto prodotto = prodottoService.modelRicercaId(prodottoId);
+		List<UnitaDiMisura> unitaMisuraList =
+			Arrays.stream(UnitaDiMisura.values()).filter(um -> um.isSonoVolume() == prodotto.getSonoVolume()).toList();
+		model.addAttribute("unitaMisuraList", unitaMisuraList);
 
-    @PostMapping("/riga")
-    public String aggiungiRiga(Model model, @RequestParam int prodottoId,
-                               @RequestParam Double quantita,
-                               @RequestParam UnitaDiMisura unitaMisura) {
-        model.addAttribute("prodottoId", prodottoId);
-        model.addAttribute("quantita", quantita);
-        model.addAttribute("unitaMisura", unitaMisura);
-        //Prodotto prodotto = prodottoService.modelRicercaId(prodottoId);
-        //List<UnitaDiMisura> unitaMisuraList = Arrays.stream(UnitaDiMisura.values())
-        //        .filter(um -> um.isSonoVolume() == prodotto.getSonoVolume())
-        //        .toList();
-        //model.addAttribute("unitaMisuraList", unitaMisuraList);
-        return "calcola-alimentazione/manuale/fragments :: prodottoDone";
-    }
+		return "calcola-alimentazione/manuale/fragments :: unitaMisura";
+	}
 
+	@PostMapping
+	@ResponseBody
+	public String salvaAlimentazione(Model model,
+		@RequestParam Long bagnoId,
+		@RequestParam List<Long> prodottoId,
+		@RequestParam List<Double> quantita,
+		@RequestParam List<UnitaDiMisura> unitaMisura) {
 
-    @DeleteMapping("/riga")
-    public String eliminaRiga(Model model) {
-        return "";
-    }
+		System.out.println(bagnoId);
+		System.out.println(prodottoId);
+		System.out.println(quantita);
+		System.out.println(unitaMisura);
+		return "";
+	}
+
+	@DeleteMapping("/riga")
+	@ResponseBody
+	public String eliminaRiga() {
+		return "";
+	}
 
 }
